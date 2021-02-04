51d8c5e5a9b67be4a9182111c0479f84c584d415
Joshua Kim <joshkim2805@gmail.com>
2021-02-04 08:54:29 -0500

    Setup some parts of snake.py

--- snake.py
+++ snake.py
@@ -1,1 +1,13 @@
 #Snake object
+class Snake():
+
+    def __init__(self, color, start_direction):
+        #maybe make color customizable by player later
+        self.x = 0
+        self.y = 0
+        #^to do: generate random coords for snake based on grid
+
+        self.positions = [(self.x, self.y), (self.x - 1, self.y - 1), (self.x - 2, self.y - 2)]
+        self.direction = start_direction
+        self.color = color
+
