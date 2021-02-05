e316b3352c7d3aaa2dfa58e559b1f860cf5d0119
Joshua Kim <joshkim2805@gmail.com>
2021-02-05 08:45:41 -0500

    Setup some attributes for apple.py

---
+++ apple.py
@@ -0,0 +1,16 @@
+#Apple object
+import pygame
+from pygame.locals import *
+
+class Apple():
+
+    def __init__(self, color, screen):
+        self.screen = screen
+        self.color = color
+        self.position = ()
+    
+    def place_on_grid(self):
+        pass
+    
+
+
