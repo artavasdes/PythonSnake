d13f8b2915f74bfea768180943014682fc138051
artavasdes <vartanyildiz@gmail.com>
2021-02-09 08:43:23 -0500

    Commit game.py

--- game.py
+++ game.py
@@ -10,6 +10,22 @@ pygame.display.set_caption("Snake game by Joshua Kim and Vartan Yildiz")
 game_over=False
 while not game_over:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -10
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = 10
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -10
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = 10
+                x1_change = 0
+        
         print(event)   #prints out all the actions that take place on the screen
  
 pygame.quit()
